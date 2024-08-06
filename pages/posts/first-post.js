b0eea7c80f06c45e08df4f020b7329050658d803
mmpotulo28 <mpotulom28@gmail.com>
2024-08-06 16:02:29 +0200

    chore: Improve responsiveness and readability of CSS styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,20 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
 	return (
 		<>
+			<Head>
+				<title>First Post</title>
+				<Script
+					src='https://connect.facebook.net/en_US/sdk.js'
+					strategy='lazyOnLoad'
+					onLoad={() => {
+						console.log('SDK loaded');
+					}}
+				/>
+			</Head>
 			<h1>First Post</h1>
 			<h2>
 				<Link href={'/'}>Back to Home</Link>
