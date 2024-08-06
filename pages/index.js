99b2183c61193d6ab2f6c1c636cb8257c63198a8
mmpotulo28 <mpotulom28@gmail.com>
2024-08-06 14:36:30 +0200

    chore: Update CSS styles for better responsiveness and readability

--- pages/index.js
+++ pages/index.js
@@ -70,15 +70,15 @@ export default function Home() {
           align-items: center;
         }
         footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
+          inline-size: 100%;
+          block-size: 100px;
+          border-block-start: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
         }
         footer img {
-          margin-left: 0.5rem;
+          margin-inline-start: 0.5rem;
         }
         footer a {
           display: flex;
