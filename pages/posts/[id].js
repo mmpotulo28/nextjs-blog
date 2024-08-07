bb8ed2f5e537c5575ebc8bcf37cab4ba8a451ef4
mmpotulo28 <mpotulom28@gmail.com>
2024-08-07 15:25:00 +0200

    chore: Improve responsiveness and readability of CSS styles

---
+++ pages/posts/[id].js
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout';
+
+export default function Posts() {
+	return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+	// return a list of possible value for id
+}
